dbe4ba739a062a3f18c4a2510063b8a8b50dd3be
priyanshuup <109269025+priyanshuup@users.noreply.github.com>
2023-08-10 21:26:34 +0530

    Update blogs.js with rents API

--- db/blogs.js
+++ db/blogs.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
 mongoose.set('strictQuery', false);
-const dbUrl = "mongodb+srv://pm:NsOFmoiNSjYsd6PW@cluster0.eplzygb.mongodb.net";
-// const dbUrl = "mongodb://127.0.0.1:27017"
-
+// const dbUrl = "mongodb+srv://pm:NsOFmoiNSjYsd6PW@cluster0.eplzygb.mongodb.net";
+const dbUrl = "mongodb://127.0.0.1:27017"
 
 const db1 = mongoose.connect(`${dbUrl}/BlogsandVideos`, {
   useNewUrlParser: true,
@@ -10,6 +9,12 @@ const db1 = mongoose.connect(`${dbUrl}/BlogsandVideos`, {
   bufferCommands: false, // Disable buffering
   // bufferMaxEntries: 0, //
 })
+.then(()=>{
+  console.log("Blogs Database Connected")
+})
+.catch((err)=>{{
+  console.log(err)
+}})
  
 
 const db2 = mongoose.createConnection(`${dbUrl}/eventssection`, {
@@ -17,20 +22,24 @@ const db2 = mongoose.createConnection(`${dbUrl}/eventssection`, {
   useUnifiedTopology: true,
 })
 
-
-// db1.once("open",()=>{
-//   console.log("Connected with Blogs Mongodb Database");
-// })
-//   .on("error",(error) => {
-//     console.log(`Error Connected with Blogs MongoDB Database,here's the error ${error}`);
-//   })
-
-
 db2.once("open",()=>{
   console.log("Connected with Events Mongodb Database");
 })
   .on("error",(error) => {
     console.log(`Error Connected with Events MongoDB Database,here's the error ${error}`);
+  })
+
+
+const db3 = mongoose.createConnection(`${dbUrl}/rentssection`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+db3.once("open",()=>{
+  console.log("Connected with Rents Mongodb Database");
+})
+  .on("error",(error) => {
+    console.log(`Error Connected with Rents MongoDB Database,here's the error ${error}`);
   })
   
 
